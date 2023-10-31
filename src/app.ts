8cad2cc83ad18ecad9fc8768885088d50387b3a6
mbndvmDevslopes <mbndvm@hotmail.com>
2023-10-31 18:19:01 -0500

    Finish getDogs

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,54 @@ import "express-async-errors";
 
 const app = express();
 app.use(express.json());
-// All code should go below this line
 
-// all your code should go above this line
+app.get("/", (_req, res) => {
+  res.json({ message: "Hello World!" }).status(200); // the 'status' is unnecessary but wanted to show you how to define a status
+});
+
+app.get("/dogs", async (req, res) => {
+  const dogs = await prisma.dog.findMany();
+  res.status(200).send(dogs);
+});
+
+app.get("/dogs/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  console.log("type of id:", typeof id);
+  if (typeof id !== "number") {
+    return res.status(400).send({
+      message: "id should be a number",
+    });
+  }
+  const dog = await prisma.dog.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!dog) {
+    res.status(204).send("No data");
+  }
+  res.status(200).send(dog);
+});
+
+app.post("/dogs", (req, res) => {
+  const body = req.body;
+});
+
+app.delete("/dogs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+});
+
+app.patch("/dogs/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const body = req.body;
+  const updatedDog = prisma.dog.update({
+    where: {
+      id,
+    },
+    data: body,
+  });
+});
 app.use(errorHandleMiddleware);
 
 const port = process.env.NODE_ENV === "test" ? 3001 : 3000;
