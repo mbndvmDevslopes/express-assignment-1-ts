f51e98d9e452ae23d7c84b4dfd469048ed5f35cd
m <mbddvm@gmail.com>
2023-11-03 04:47:51 -0500

    Workig on delete

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { prisma } from "../prisma/prisma-instance";
 import { errorHandleMiddleware } from "./error-handler";
 import "express-async-errors";
+import { PrismaClientValidationError } from "@prisma/client/runtime";
 
 const app = express();
 app.use(express.json());
@@ -17,8 +18,7 @@ app.get("/dogs", async (req, res) => {
 
 app.get("/dogs/:id", async (req, res) => {
   const id = parseInt(req.params.id);
-  console.log("type of id:", typeof id);
-  if (typeof id !== "number") {
+  if (isNaN(id)) {
     return res.status(400).send({
       message: "id should be a number",
     });
@@ -35,24 +35,55 @@ app.get("/dogs/:id", async (req, res) => {
   res.status(200).send(dog);
 });
 
-app.post("/dogs", (req, res) => {
+/* app.post("/dogs", (req) => {
   const body = req.body;
 });
-
-app.delete("/dogs/:id", (req, res) => {
+ */
+app.delete("/dogs/:id", async (req, res) => {
   const id = parseInt(req.params.id);
+  try {
+    const deletedDog = await prisma.dog.delete({
+      where: {
+        id,
+      },
+    });
+    res.status(200).send(deletedDog);
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-app.patch("/dogs/:id", (req, res) => {
+app.patch("/dogs/:id", async (req, res) => {
   const id = parseInt(req.params.id);
-  const body = req.body;
-  const updatedDog = prisma.dog.update({
-    where: {
-      id,
-    },
-    data: body,
-  });
+  const data = req.body;
+
+  try {
+    const updatedDog = await prisma.dog.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    res.status(201).send(updatedDog);
+  } catch (error) {
+    if (error instanceof PrismaClientValidationError) {
+      console.error(
+        "Prisma Validation Error:",
+        error.message
+      );
+      res
+        .status(400)
+        .send({ error: "Invalid update data" });
+    } else {
+      console.error("Error updating dog:", error);
+      res
+        .status(500)
+        .send({ error: "Internal Server Error" });
+    }
+  }
 });
+
+
 app.use(errorHandleMiddleware);
 
 const port = process.env.NODE_ENV === "test" ? 3001 : 3000;
